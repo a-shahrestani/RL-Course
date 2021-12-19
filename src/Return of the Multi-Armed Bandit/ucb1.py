6f67c2a647bec87020c874d1e1e6266173a63367
Afshin <afshinshah77@gmail.com>
2021-12-19 10:09:57 +0330

    ucb1 method added and tested

---
+++ src/Return of the Multi-Armed Bandit/ucb1.py
@@ -0,0 +1,89 @@
+# From the course: Bayesin Machine Learning in Python: A/B Testing
+# https://deeplearningcourses.com/c/bayesian-machine-learning-in-python-ab-testing
+# https://www.udemy.com/bayesian-machine-learning-in-python-ab-testing
+from __future__ import print_function, division
+
+import math
+import random
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+from math import sqrt
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+NUM_TRIALS = 10000
+# EPS = 0.1
+BANDIT_PROBABILITIES = [0.2 , 0.5, 0.75]
+
+
+class Bandit:
+    def __init__(self, p):
+        # p: the win rate
+        self.p = p
+        self.p_estimate = 0
+        self.N = 1
+
+    def pull(self):
+        # draw a 1 with probability p
+        return np.random.random() < self.p
+
+    def update(self, x):
+        self.N += 1
+        self.p_estimate = (((self.N - 1) * self.p_estimate) + x) / self.N
+
+
+def experiment(BANDIT_PROBABILITIES=None):
+    bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+
+    rewards = np.zeros(NUM_TRIALS)
+    num_times_explored = 0
+    num_times_exploited = 0
+    num_optimal = 0
+    optimal_j = np.argmax([b.p for b in bandits])
+    print("optimal j:", optimal_j)
+    for bandit in range(len(bandits)):
+        x = bandits[bandit].pull()
+        rewards[bandit] = x
+        bandits[bandit].update(x)
+        if bandit == optimal_j:
+            num_optimal += 1
+
+    for i in range(len(bandits),NUM_TRIALS):
+
+        j = np.argmax([(b.p_estimate + sqrt(2*math.log2(i)/b.N)) for b in bandits])
+
+        if j == optimal_j:
+            num_optimal += 1
+
+        # pull the arm for the bandit with the largest sample
+        x = bandits[j].pull()
+
+        # update rewards log
+        rewards[i] = x
+
+        # update the distribution for the bandit whose arm we just pulled
+        bandits[j].update(x)
+
+    # print mean estimates for each bandit
+    for b in bandits:
+        print("mean estimate:", b.p_estimate)
+
+    # print total reward
+    print("total reward earned:", rewards.sum())
+    print("overall win rate:", rewards.sum() / NUM_TRIALS)
+    print("num of times each bandit was chosen:", [b.N for b in bandits])
+
+    # plot the results
+    cumulative_rewards = np.cumsum(rewards)
+    win_rates = cumulative_rewards / (np.arange(NUM_TRIALS) + 1)
+    plt.ylim([0, 1])
+    plt.plot(win_rates)
+    BANDIT_PROBABILITIES = [probability[0] for probability in BANDIT_PROBABILITIES]
+    plt.plot(np.ones(NUM_TRIALS) * np.max(BANDIT_PROBABILITIES))
+    plt.show()
+
+
+if __name__ == "__main__":
+    experiment(BANDIT_PROBABILITIES)
