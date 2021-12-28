412991e9103aeeaff346e7497665cb37df4ffb1c
Afshin <afshinshah77@gmail.com>
2021-12-28 14:01:35 +0330

    gridworld changed - penalized actions added policy iteration added - tested for probabilistic and penalized actions

--- src/Dynamic Programming/policy_iteration_probablistic.py
+++ src/Dynamic Programming/policy_iteration_probablistic.py
@@ -1,4 +1,4 @@
-from gridworld import standard_windy_grid, ACTION_SPACE
+from gridworld import standard_windy_grid_penalized, ACTION_SPACE, standard_windy_grid
 import numpy as np
 
 
@@ -35,16 +35,6 @@ def print_values(v, g):
 def transition_fill(grid, ACTION_SPACE):
     transition_probs = {}
     rewards = {}
-    # for i in range(grid.rows):
-    #     for j in range(grid.columns):
-    #         s = (i, j)
-    #         if not grid.is_terminal(s):
-    #             for a in ACTION_SPACE:
-    #                 if (s,a) in grid.probs.keys():
-    #                     for next_possible_state in grid.probs.get((s, a)):
-    #                         transition_probs[(s, a, next_possible_state[0])] = next_possible_state[1]
-    #                         if next_possible_state in grid.rewards:
-    #                             rewards[(s, a, next_possible_state[0])] = grid.rewards.get(next_possible_state[0],0)
     for (s, a), v in grid.probs.items():
         for s2, p in v.items():
             transition_probs[(s, a, s2)] = p
@@ -52,25 +42,7 @@ def transition_fill(grid, ACTION_SPACE):
     return transition_probs, rewards
 
 
-if __name__ == '__main__':
-    policy = {
-        (2, 0): {'U': 0.5, 'R': 0.5},
-        (1, 0): {'U': 1.0},
-        (0, 0): {'R': 1.0},
-        (0, 1): {'R': 1.0},
-        (0, 2): {'R': 1.0},
-        (1, 2): {'U': 1.0},
-        (2, 1): {'R': 1.0},
-        (2, 2): {'U': 1.0},
-        (2, 3): {'L': 1.0},
-    }
-    grid = standard_windy_grid()
-    V = {s: 0.0 for s in grid.all_states()}
-    # print_policy(policy, grid)
-    transition_probs, rewards = transition_fill(grid, ACTION_SPACE)
-    print_policy(policy, grid)
-    gamma = 0.9
-    threshold = 1e-3
+def policy_evaluation(policy, grid, V, transition_probs, rewards, gamma, threshold):
     it = 0
     while True:
         biggest_change = 0
@@ -80,14 +52,52 @@ if __name__ == '__main__':
                 new_v = 0
                 for a in ACTION_SPACE:
                     for s2 in grid.all_states():
-                        action_prob = policy[s].get(a, 0)
+                        action_prob = 1 if policy.get(s) == a else 0
                         r = rewards.get((s, a, s2), 0)
                         new_v += action_prob * transition_probs.get((s, a, s2), 0) * (r + gamma * V[s2])
                 V[s] = new_v
                 biggest_change = max(biggest_change, np.abs(old_v - V[s]))
-        print("iter: ", it, 'biggest change:', biggest_change)
+        # print("iter: ", it, 'biggest change:', biggest_change)
         print_values(V, grid)
         it += 1
         if biggest_change < threshold:
             break
-    print('\n\n')
+
+
+if __name__ == '__main__':
+    # grid = standard_windy_grid()
+    grid = standard_windy_grid_penalized(-0.1)
+    policy = {}
+    for s in grid.actions.keys():
+        policy[s] = np.random.choice(ACTION_SPACE)
+    V = {s: 0.0 for s in grid.all_states()}
+    # print_policy(policy, grid)
+    transition_probs, rewards = transition_fill(grid, ACTION_SPACE)
+    print_policy(policy, grid)
+    gamma = 0.9
+    threshold = 1e-3
+    while True:
+        is_policy_converged = True
+        policy_evaluation(policy, grid, V, transition_probs, rewards, gamma, threshold)
+        for s in grid.actions.keys():
+            old_a = policy[s]
+            new_a = None
+            best_value = float('-inf')
+            for a in ACTION_SPACE:
+                v = 0
+                for s2 in grid.all_states():
+                    r = rewards.get((s, a, s2), 0)
+                    v += transition_probs.get((s, a, s2), 0) * (r + gamma * V[s2])
+                if v > best_value:
+                    best_value = v
+                    new_a = a
+            policy[s] = new_a
+            if new_a != old_a:
+                is_policy_converged = False
+        if is_policy_converged:
+            break
+
+    print("velues:")
+    print_values(V, grid)
+    print("policy:")
+    print_policy(policy, grid)
